de12334d24d4532a8a4a3442f32b84327d110970
LuisLuna810 <luis.agustin10@hotmail.com>
2024-07-19 11:27:38 -0300

    update dependencies

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = require('./src/app.js');
 
 require('dotenv').config();
 
-const { REPOSITORIOS } = process.env;
 const port = process.env.PORT || 4000;
 
 const httpServer = http.createServer(app);
@@ -12,12 +11,3 @@ const httpServer = http.createServer(app);
 httpServer.listen(port, () => {
   console.log(`Server listening at ${port}`);
 })
-
-// db.sync({ alter: true })
-//   .then(() => {
-//     console.log('Database synced successfully');
-
-//   })
-//   .catch((error) => {
-//     console.error('Error syncing database:', error);
-//   });
